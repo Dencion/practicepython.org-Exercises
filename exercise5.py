858d2f4e0ab10570a9c44dab9ab03a9db3144e75
Dencion <37498539+Dencion@users.noreply.github.com>
2018-03-18 22:29:01 +1100

    exercise5.py

---
+++ exercise5.py
@@ -0,0 +1,11 @@
+#Exercise 5
+#Ask the user for a string and print out whether this string is a
+#palindrome or not. 
+
+userWord = input("Please enter a word:")
+
+if userWord == userWord[::-1]:
+    print("Your word is a palindrome")
+else:
+    print("Your word aint a palindrome")
+
